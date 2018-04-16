2627f10cd1e2da6368d5dd6ac9cfe645c2b6319d
Bas Kiers <kiers.bas@gmail.com>
2018-04-16 16:37:10 +0200

    Rewrite to SDK2

---
+++ drivers/lg_tv/device.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Homey = require('homey');
+const util = require('homey-rfdriver').util;
+
+module.exports = RFDevice => class MyDevice extends RFDevice {
+
+	onInit() {
+		super.onInit();
+
+		if (this.isPairInstance) {
+			this.setDeviceState({ data: { id: this.getDriver().id } });
+		}
+	}
+
+	getSendOptionsForCmdObject(cmdObj, options) {
+		if (cmdObj.cmd.startsWith('VOLUME_')) {
+			return Object.assign(options, { repetitions: 2 });
+		}
+		return options;
+	}
+
+};
